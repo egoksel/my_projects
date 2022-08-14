1322c1ee8d3e6bfc3fe68a36ec9e58534c010578
emrah <egoksel250@gmail.com>
2022-08-14 13:55:55 +0300

    2

--- flask-02-jinja-template/jinja.py
+++ flask-02-jinja-template/jinja.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def head():
-    return render_template('index.html', number1 = 7000, number2 = 9000)
+    return render_template('index.html', number1 = 1250, number2 = 8850)
 if __name__ == '__main__':
     app.run(debug=True)
