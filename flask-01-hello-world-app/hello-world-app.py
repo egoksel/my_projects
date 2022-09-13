ef2c9e8e882e1c44ecb20996c36f27b363185e30
emrah <egoksel250@gmail.com>
2022-09-13 06:49:30 +0300

    hello

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -2,15 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello World from Flask!!!"
+    return "Hello World"
+
 @app.route('/second')
 def second():
-    return 'Bize Her Yer Trabzon!!!!'
+    return "This is te second page"
+
 @app.route('/third/subthird')
 def third():
-    return 'This is the subpage of third page'
+    return "This is the subpage of third page"
+
 @app.route('/fourth/<string:id>')
 def fourth(id):
-    return f'Id number of this page is {id}'
+    return f'Id number of this page is : {1250}'
+
 if __name__ == '__main__':
     app.run(debug=True, port=2000)
